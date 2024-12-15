a2c855cc58d2268ba495440776ff4a32f37e92a3
nisha783 <hm6980097@gmail.com>
2024-12-16 00:00:53 +0500

    about or contact chng

--- resources/views/landing/about.blade.php
+++ resources/views/landing/about.blade.php
@@ -7,40 +7,41 @@
             <div class="breadcumb-menu-wrap">
                 <ul class="breadcumb-menu">
                     <li><a href="{{ route('index') }}">Home</a></li>
-                    <li>About Us</li>
+                    <li style="color:  #1f6f78;">About Us</li>
                 </ul>
             </div>
         </div>
     </div>
 </div>
 
-<section class="space-top mb-30">
+<section class=" space-top mb-30">
     <div class="container">
-        <div class="row gx-60 align-items-center">
-            <div class="col-xl-6 mb-30 mb-xl-0 wow fadeInUp" data-wow-delay="0.3s">
-                <div class="img-box5">
-                    <div class="img-1"><img src="{{ asset('landing/img/about.jpg') }}" alt="about"></div>
-                    <div class="shape-1"></div>
-                </div>
-            </div>
+        <div class="row gx-60">
             <div class="col-xl-6 wow fadeInUp" data-wow-delay="0.4s">
-                <div class="sec-pills">
+                <div class="sec-pills" style="color: #1f6f78;">
                     <div class="pill"></div>
                     <div class="pill"></div>
                     <div class="pill"></div>
                 </div>
-                <span class="sec-subtitle3">Invest with confidence, trade with experts - MarkSoulDigital.</span>
-                <h2 class="sec-title2 mb-2 mb-xxl-3 pb-1">Transcending borders, maximizing profits</h2>
-                <p class="about-text1 mb-xl-3 mb-xxl-4 pb-2">Welcome to MarkSoulDigital (MSD), your trusted platform for
-                    forex trading excellence. We specialize in empowering investors like you to capitalize on the vast
-                    potential of the forex market. Our cutting-edge platform brings together skilled traders and
-                    ambitious investors seeking financial growth and success. At MSD, we believe in transparency,
-                    innovation, and a commitment to our clients' prosperity.
+                <span class="sec-subtitle3" style="color: #1f6f78;">Unlock the full potential of stock markets alongside our experts.</span>
+                <h2 class="sec-title2 mb-2 mb-xxl-3 pb-1">MSP: Where successful stock journeys begin.</h2>
+                <p class="about-text1 mb-xl-3 mb-xxl-4 pb-2">We stand out as a premier choice for stock trading due to
+                    our unwavering commitment to our clients' success and the exceptional value we bring to the table.
+                    Our team of expert traders is driven by a passion for excellence and an in-depth understanding of
+                    the stock market's intricacies. With a proven track record of delivering consistent profits and a
+                    transparent approach, we offer a reliable and rewarding platform for investors to capitalize on the
+                    vast potential of Stock trading.
                 </p>
                 <div class="row  align-items-center justify-content-end flex-row-reverse mt-4 mt-xxl-5 pt-3 pt-xl-1 ">
                     <div class="col-sm-auto">
-                        <a href="{{ route('register') }}" class="vs-btn style7">Create Account</a>
+                        <a href="{{ route('register', ['refer' => 'admin', 'position' => 'left']) }}"
+                            class="vs-btn style7" style="background-color: #1f6f78;">Create Account</a>
                     </div>
+                </div>
+            </div>
+            <div class="col-xl-6 mb-30 mb-xl-0 wow fadeInUp" data-wow-delay="0.3s">
+                <div class="img-box5">
+                    <img src="{{ asset('landing/img/401376.jpg') }}" class="rounded" style="border-radius: 12px;" alt="about">
                 </div>
             </div>
         </div>
@@ -49,7 +50,7 @@
 
 <div class=" space-bottom">
     <div class="container">
-        <div data-bg-src="landing/img/shape/counter-shape-1-1.jpg">
+        <div data-bg-src="" style="background-color: #1f6f78;">
             <div class="row gx-0">
                 <div class="col-md-6 col-lg vs-counter  wow fadeInUp" data-wow-delay="0.3s">
                     <div class="vs-counter__number">
@@ -84,34 +85,34 @@
     </div>
 </div>
 
-<section class=" space-top">
+<section class="space-top">
     <div class="container">
         <div class="row gx-60">
+            <div class="col-xl-6 mb-30 mb-xl-0 wow fadeInUp" data-wow-delay="0.3s">
+                <div class="img-box5">
+                    <img src="{{ asset('landing/img/52480.jpg') }}" class="rounded" style="border-radius:30px;" alt="about">
+                </div>
+            </div>
             <div class="col-xl-6 wow fadeInUp" data-wow-delay="0.4s">
-                <div class="sec-pills">
+                <div class="sec-pills" style="color:#1f6f78;">
                     <div class="pill"></div>
                     <div class="pill"></div>
                     <div class="pill"></div>
                 </div>
-                <span class="sec-subtitle3">Unlock the full potential of forex markets alongside our experts.</span>
-                <h2 class="sec-title2 mb-2 mb-xxl-3 pb-1">MSD: Where successful forex journeys begin.</h2>
-                <p class="about-text1 mb-xl-3 mb-xxl-4 pb-2">We stand out as a premier choice for forex trading due to
-                    our unwavering commitment to our clients' success and the exceptional value we bring to the table.
-                    Our team of expert traders is driven by a passion for excellence and an in-depth understanding of
-                    the forex market's intricacies. With a proven track record of delivering consistent profits and a
-                    transparent approach, we offer a reliable and rewarding platform for investors to capitalize on the
-                    vast potential of forex trading.
+                <span class="sec-subtitle3" style="color: #1f6f78;">Invest with confidence, trade with experts - MetaStockPair.</span>
+                <h2 class="sec-title2 mb-2 mb-xxl-3 pb-1">Transcending borders, maximizing profits</h2>
+                <p class="about-text1 mb-xl-3 mb-xxl-4 pb-2">Welcome to MetaStockPair (MSP), your trusted platform
+                    for
+                    stock trading excellence. We specialize in empowering investors like you to capitalize on the vast
+                    potential of the stock market. Our cutting-edge platform brings together skilled traders and
+                    ambitious investors seeking financial growth and success. At MSP, we believe in transparency,
+                    innovation, and a commitment to our clients' prosperity.
                 </p>
                 <div class="row  align-items-center justify-content-end flex-row-reverse mt-4 mt-xxl-5 pt-3 pt-xl-1 ">
                     <div class="col-sm-auto">
-                        <a href="{{ route('register') }}" class="vs-btn style7">Create Account</a>
+                        <a href="{{ route('register', ['refer' => 'admin', 'position' => 'left']) }}"
+                            class="vs-btn style7" style="background-color: #1f6f78;">Create Account</a>
                     </div>
-                </div>
-            </div>
-            <div class="col-xl-6 mb-30 mb-xl-0 wow fadeInUp" data-wow-delay="0.3s">
-                <div class="img-box5">
-                    <div class="img-1"><img src="{{ asset('landing/img/why.jpg') }}" alt="about"></div>
-                    <div class="shape-1"></div>
                 </div>
             </div>
         </div>
